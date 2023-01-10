30ea3c96acb58b0d3cd24afde7f8801d711b15af
karfaisarra <karfaisarra@gmail.com>
2023-01-10 17:52:58 +0100

    add function index

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.admin')
+@section('content')
+<div class="table-responsive">
+    <table class="table table-striped
+    table-hover	
+    table-borderless
+    table-primary
+    align-middle">
+        <thead class="table-light">
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Slug</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody class="table-group-divider">
+            @forelse($projects as $project)
+            <tr class="table-primary">
+                <td scope="row">{{$project->id}}</td>
+                <td>{{$project->title}}</td>
+                <td>{{$project->slug}}</td>
+                <td>
+                    <i class="fas fa-eye fa-sm fa-fw"></i>
+                    <i class="fas fa-pencil fa-sm fa-fw"></i>
+                    <i class="fas fa-trash fa-sm fa-fw"></i>
+                </td>
+            </tr>
+            @empty
+            <tr>
+                <td>No Projects Yet!</td>
+            </tr>
+            @endforelse
+        </tbody>
+        <tfoot>
+
+        </tfoot>
+    </table>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,8 +24,10 @@ Route::middleware(['auth', 'verified'])->name('dashboard')
     ->name('admin.')
     ->prefix('admin')
     ->group(function () {
-        Route::get('/', [DashboardController::class, 'index'])
-            ->name('dashboard');
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('projects', ProjectController::class)->parameters([
+            'projects' => 'project:slug'
+        ]);
     });
 
 require __DIR__ . '/auth.php';
