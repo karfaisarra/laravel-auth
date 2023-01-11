09a3711ce85fb801a2b144f532d051a361971aad
karfaisarra <karfaisarra@gmail.com>
2023-01-11 12:15:31 +0100

    add changes

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -17,11 +17,11 @@
 
     <div class="mb-3">
         <label for="" class="form-label">Title</label>
-        <input type="text" name="title" id="title" class="form-control" placeholder="add title" aria-describedby="titleHelper">
+        <input type="text" name="title" id="title" class="form-control @error('title') is-invalid @enderror" placeholder="add title" aria-describedby="titleHelper">
         <small id="titleHelper" class="text-muted">must be unique, max 100 charecters</small>
     </div>
     <div class="mb-3">
-        <label for="description" class="form-label">description</label>
+        <label for="description" class="form-label @error('description') is-invalid @enderror">description</label>
         <textarea name="description" id="description" class="form-control" rows="5"></textarea>
     </div>
     <button type="submit" class="btn btn-primary">Add</button>
